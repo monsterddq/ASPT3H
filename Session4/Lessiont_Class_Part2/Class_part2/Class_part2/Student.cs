39d5e16b88b0cf07e331e70065cac40277f03d6e
monterddq <thaidq@d3plus.com>
2017-11-01 17:45:35 +0700

    Class and Method

---
+++ Session4/Lessiont_Class_Part2/Class_part2/Class_part2/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_part2
+{
+    public class Student: Person
+    {
+        public String Grade { get; set; }
+
+        
+
+        public void PrintAge() {
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Session4/Lessiont_Class_Part2/Class_part2/Class_part2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Class_part2;
+
+namespace Class_part2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em = new Employee();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
