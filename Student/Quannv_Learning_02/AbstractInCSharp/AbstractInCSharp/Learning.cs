b7eb7e5528f765d3abe2b47f02ca5da642f82205
MonsterDDQ <thaidq@d3plus.com>
2017-11-22 18:06:45 +0700

    Update

---
+++ Student/Quannv_Learning_02/AbstractInCSharp/AbstractInCSharp/Learning.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractInCSharp
+{
+    abstract class Learning
+    {
+        string content {get;set;}
+
+        public Learning(string content)
+        {
+            this.content = content;
+        }
+
+        public Learning()
+        {
+        }
+    }
+}
