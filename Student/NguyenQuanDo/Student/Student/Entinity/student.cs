1f0fd53eecc20555da9a8502258f6e7f6c8e641c
root <thaidq@d3plus.com>
2017-12-04 18:02:12 +0700

    Entity Framework Database First

---
+++ Student/NguyenQuanDo/Student/Student/Entinity/student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Student.Entinity
+{
+    public class student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set;}
+        public string StudentPhone { get; set; }
+        public student(int StudentId, string StudentName, String StudentPhone)
+        {
+            this.StudentId = StudentId;
+            this.StudentName = StudentName;
+            this.StudentPhone = StudentPhone;
+        }
+        public student()
+        {
+
+        }
+    }
+}
---
+++ Student/VuDinhTrong/StudentApp/StudentApp/ICommonObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+
+namespace StudentApp
+{
+    internal interface ICommonObject<T, K>
+    {
+        List<T> GetAll();
+
+        void AddNew(T obj);
+
+        void Modify(T obj);
+
+        void Delete(K key);
+    }
+}
